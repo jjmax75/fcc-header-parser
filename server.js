8268b067a6514b8d45b9bec6fbcdf7cb39d5234e
John Behan <behanj@gmail.com>
2016-03-04 14:41:06 +0700

    express trust proxy enabled

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var port = process.env.PORT || 8080;
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
+app.enable('trust proxy'); // get true IP address
+
 routes(app);
 
 app.listen(port,  function () {
